97cc66f3df5317ff0a21a71c0059cc5797d9f9e6
Bernhard Häussermann <bernhard.haeussermann@absa.africa>
2021-01-06 07:48:38 +0200

    Implemented tree-shaking for smaller production bundle sizes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.s(c|a)ss$/,
-          use: [
-            'vue-style-loader',
-            'css-loader'
-            {
-              loader: 'sass-loader',
-              options: {
-                implementation: require('sass'),
-                sassOptions: {
-                  indentedSyntax: true // optional
-                }
+  module: {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                indentedSyntax: true // optional
               }
             }
-          ]
-        }
-      ]
-    }
-  };
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VuetifyLoaderPlugin()
+  ]
+};
